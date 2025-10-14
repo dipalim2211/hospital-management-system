9bf5becad270a7abf49508e46c663b996317ddab
dipalim2211 <dipalimanere22@gmail.com>
2025-10-14 22:14:17 +0530

    query for add patient

--- hospital-management/src/com/hospitalManagementSystem/Patient.java
+++ hospital-management/src/com/hospitalManagementSystem/Patient.java
@@ -1,15 +1,18 @@
 package com.hospitalManagementSystem;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Patient {
 
-    private Connection connection;
+    //Encapsulation
+    private Connection connection;  //take value from main class
 
     private Scanner sc;
 
+    //Parameterized constructor
     public Patient(Connection connection, Scanner sc) {
         this.connection = connection;
         this.sc = sc;
@@ -25,10 +28,21 @@ public class Patient {
         System.out.println("Gender :  ");
         String gender = sc.next();
 
+        //try-catch : to avoid sql exception while connecting to db using connection Interface
+
         try{
-        String query ="Insert into ";
-        }catch (SQLException e){
-        e.printStackTrace();
+            String query = "Insert into patients(name,age,gender)values(?,?,?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1,name);
+            preparedStatement.setInt(2,age);
+            preparedStatement.setString(3,gender);
+            int affectedRows=preparedStatement.executeUpdate();
+            if(affectedRows>0){
+                System.out.println("Patient Added Successfully");
+            }else
+                System.out.println("Failed to add patient");
+        }catch(SQLException e){
+            e.printStackTrace();
         }
     }
 
