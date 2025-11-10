e6f87c12f8a85682e2034b79d5fcc60d7f51df9d
dipalim2211 <dipalimahesh2211@gmail.com>
2025-11-10 22:21:12 +0530

    function to call Doctor by ID added

--- hospital-management/src/com/hospitalManagementSystem/Doctor.java
+++ hospital-management/src/com/hospitalManagementSystem/Doctor.java
@@ -3,6 +3,7 @@ package com.hospitalManagementSystem;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Doctor {
@@ -37,12 +38,30 @@ public class Doctor {
                 String specialization = resultSet.getString("specialization");
 
                 System.out.print(id+name+specialization);
-
             }
-
         }catch (Exception e){
             e.printStackTrace();
         }
+    }
+
+
+    //Retrieve doctors using ID
+
+    public boolean getDoctorsById(int id){
+        String query ="select * from patients where id = ?";
+        try{
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1,id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                return true;
+            }else {
+                return false;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
 }
