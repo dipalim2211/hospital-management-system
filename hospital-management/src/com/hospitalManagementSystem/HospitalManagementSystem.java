84fbf5219c68770e557d161c3256770e9efad81f
dipalim2211 <dipalimahesh2211@gmail.com>
2025-11-11 22:14:39 +0530

    connection object created

--- hospital-management/src/com/hospitalManagementSystem/HospitalManagementSystem.java
+++ hospital-management/src/com/hospitalManagementSystem/HospitalManagementSystem.java
@@ -1,5 +1,9 @@
 package com.hospitalManagementSystem;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class HospitalManagementSystem {
 
     /*
@@ -13,5 +17,23 @@ public class HospitalManagementSystem {
     private static final String username = "root";
 
     private static final String password= "Java@2211";
+
+    public static void main(String[] args) {
+        try{
+            //Load all driver required to connect database
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+
+        try{
+            //make connection with db
+            Connection connection = DriverManager.getConnection(url,username,password);
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+
 
 }
